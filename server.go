62038a7f2b4c3039d7f6b8a36064a27e717c537e
Gary Capell <gary.capell@gmail.com>
2012-06-10 01:24:17 +1000

    Simplify by using http.ServeFile

--- server.go
+++ server.go
@@ -1,37 +1,20 @@
 package main
 
 import (
-	"net/http"
 	"log"
-	"os"
-	"io"
-	"flag"
+	"net/http"
 )
 
 func tree(w http.ResponseWriter, r *http.Request) {
 	log.Printf("\n\nTREE: %v\n", *r)
-	fp, err := os.Open("blah.js")
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-	io.Copy(w, fp)
+	http.ServeFile(w, r, "blah.js")
 }
 
 func index(w http.ResponseWriter, r *http.Request) {
-
-	req := r.URL.Path[1:]
-	fp, err := os.Open(req)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-	written, err := io.Copy(w, fp)
-	log.Println(req, written, err)
+	http.ServeFile(w, r, r.URL.Path[1:])
 }
 
 func main() {
-	flag.Parse()
 	http.HandleFunc("/", index)
 	http.HandleFunc("/tree", tree)
 	http.ListenAndServe(":8080", nil)
