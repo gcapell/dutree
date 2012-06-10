89b069c58f91b93e2b77b7e2e2e78ac7900e8654
Gary Capell <gary.capell@gmail.com>
2012-06-10 12:14:09 +1000

    random change, sorry

--- blah.js
+++ blah.js
@@ -1,11 +1,7 @@
 [
-	{
-		"data" : "A node", 
-		"children" : [
-			{ "data" : "Only child", "state" : "open" },
-			{ "data" : "second child", "state" : "closed" }
-			
-		],
-		"state" : "open"
-	}
+	{"data" : "A node", "attr": {"id": "123"}},
+	{ "data": "stuff", "state": "open", "attr": {"id": "2"}},
+	{ "data": "stuff2", "state": "closed", "attr": {"id": "3"}},
+	{ "data": "stuff3", "attr": {"id": "4"}},
+	{ "data": "stuff4", "attr": {"id": "5"}}
 ]
--- my.js
+++ my.js
@@ -2,21 +2,30 @@ $(document).ready(function() {
 		$("#tree1").jstree({
 		plugins : [ "themes", "json_data"],
 		core : {animation: 0},
+		"themes" : {
+			"icons" : false
+		},
 		"json_data": {
 			"ajax": {
-				"url": function(n) {
-					console.log("url",n);
-					return "/tree";
-				},
+				"url": "/tree",
 				"data": function(n) {
+					console.log("n", n);
 			        // the result is fed to the AJAX request 'data' option
-			        var reply = {
-						"id": "fish"
-			            // "id": n.attr ? n.attr("id").replace("node-", "") : 1,
-			        };
-		
-					console.log("data", reply, "n", n);
-					return reply;
+					if (n.attr) {
+						console.log("got attr");
+						if (n.attr("id")) {
+							console.log("got id");
+							var retval = {"id": n.attr("id").replace("node-", "")};
+							console.log("retval", retval);
+							return retval;
+						} else {
+							console.log("no id");
+						}
+					} else {
+						console.log("no attr");
+					}
+					console.log("-1");
+					return {"id": -1};
 			    },
 			}
 		},
