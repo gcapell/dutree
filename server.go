ccc9001a1c7f3080b09b4d983be00696ae680367
Gary Capell <gary.capell@gmail.com>
2012-06-10 15:16:58 +1000

    Works, a bit

--- server.go
+++ server.go
@@ -3,13 +3,15 @@ package main
 import (
 	"encoding/json"
 	"flag"
+	"fmt"
 	"log"
 	"net/http"
+	"strconv"
 )
 
 type (
 	Attr struct {
-		Id string `json:"id"`
+		Id NodeID `json:"id"`
 	}
 	Node struct {
 		Data  string `json:"data"`
@@ -18,15 +20,41 @@ type (
 	}
 )
 
-func tree(w http.ResponseWriter, r *http.Request) {
-	log.Printf("TREE: %v\n", r.URL)
+func tree(w http.ResponseWriter, r *http.Request, store Storage, root NodeID) {
+	log.Printf("TREE: %v => %s\n", r.URL, r.FormValue("id"))
 
-	n := []Node{
-		{"c", "d", Attr{"3"}},
-		{"a", "closed", Attr{"1"}},
+	id, err := strconv.ParseUint(r.FormValue("id"), 10, 64)
+	var nodeID NodeID
+	if err != nil {
+		nodeID = root
+	} else {
+		nodeID = NodeID(id)
+	}
+	node, err := store.Retrieve(nodeID)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
 	}
 
-	b, err := json.Marshal(n)
+	children := make([]Node, len(node.children))
+	for j, c := range node.children {
+		cval, err := store.Retrieve(c)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		var state string
+		if len(cval.children) != 0 {
+			state = "closed"
+		} else {
+			state = "x"
+		}
+		children[j] = Node{
+			fmt.Sprintf("%s %d", cval.path, cval.data),
+			state, Attr{cval.id}}
+	}
+
+	b, err := json.Marshal(children)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
@@ -49,7 +77,9 @@ func main() {
 	log.Printf("store: %v", store)
 
 	http.HandleFunc("/", index)
-	http.HandleFunc("/tree", tree)
+	http.HandleFunc("/tree", func(w http.ResponseWriter, r *http.Request) {
+		tree(w, r, store, data.id)
+	})
 	addr := ":8080"
 	log.Printf("Listening on", addr)
 	http.ListenAndServe(addr, nil)
