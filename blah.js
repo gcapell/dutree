c527c8d8b6d697ea609862da57729b4fa32f1f3b
Gary Capell <gary.capell@gmail.com>
2012-06-10 13:09:40 +1000

    Add json-encoding

--- blah.js
+++
@@ -1,7 +0,0 @@
-[
-	{"data" : "A node", "attr": {"id": "123"}},
-	{ "data": "stuff", "state": "open", "attr": {"id": "2"}},
-	{ "data": "stuff2", "state": "closed", "attr": {"id": "3"}},
-	{ "data": "stuff3", "attr": {"id": "4"}},
-	{ "data": "stuff4", "attr": {"id": "5"}}
-]
--- server.go
+++ server.go
@@ -3,11 +3,33 @@ package main
 import (
 	"log"
 	"net/http"
+	"encoding/json"
+)
+
+type (
+	Attr struct {Id string `json:"id"`}
+	Node struct {
+		Data string `json:"data"`
+		State string `json:"state"`
+		Attr Attr `json:"attr"`
+	}
 )
 
 func tree(w http.ResponseWriter, r *http.Request) {
-	log.Printf("\n\nTREE: %v\n", *r)
-	http.ServeFile(w, r, "blah.js")
+	log.Printf("TREE: %v\n", r.URL)
+
+	n := []Node {
+		{"c", "d",  Attr{"3"}},
+		{"a", "closed",  Attr{"1"}},
+	}
+
+	b, err := json.Marshal(n)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	log.Printf("returning: %v\n", string(b))
+	w.Write(b)
 }
 
 func index(w http.ResponseWriter, r *http.Request) {
